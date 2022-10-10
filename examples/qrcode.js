fac2137b8351ec04ecb8a70cd2e900a9541cf82f
Cristiano Cruz <cristiano@linvix.com.br>
2022-10-10 11:32:50 -0300

    ajustes

--- examples/qrcode.js
+++ examples/qrcode.js
@@ -5,6 +5,7 @@ const { Printer, Align, Model, InMemory } = require('../dist');
   const connection = new InMemory()
   const printer = await Printer.CONNECT(model, connection)
   await printer.setAlignment(Align.Center)
+  // not all printers have the qrcode method implemented, you may need to implement the method with the manufacturer's esc/pos commands.
   await printer.qrcode('https://github.com/grandchef/escpos-buffer')
   await printer.setAlignment(Align.Left)
   await printer.buzzer()

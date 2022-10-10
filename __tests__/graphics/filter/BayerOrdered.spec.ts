8cbaf971d721edaa8c7cce6c1bc77218b5ac63b2
Cristiano Cruz <cristiano@linvix.com.br>
2022-10-10 08:29:37 -0300

    remoções

--- __tests__/graphics/filter/BayerOrdered.spec.ts
+++
@@ -1,9 +0,0 @@
-import { load } from '../../helper';
-import { BayerOrdered, Image } from '../../../src';
-
-describe('proccess images using Bayer ordered algorithm', () => {
-  it('apply filter on image from buffer', () => {
-    const image = new Image(load('sample.png'), new BayerOrdered());
-    expect(image.data).toStrictEqual(load('bayer_filter', image.data));
-  });
-});
--- __tests__/graphics/filter/Threshold.spec.ts
+++
@@ -1,9 +0,0 @@
-import { load } from '../../helper';
-import { Threshold, Image } from '../../../src';
-
-describe('proccess images using threshold algorithm', () => {
-  it('apply filter on image from buffer', () => {
-    const image = new Image(load('sample.png'), new Threshold());
-    expect(image.data).toStrictEqual(load('threshold_filter', image.data));
-  });
-});
